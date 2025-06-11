e1c5f81e6a11987c58d817d17abc2cd1025e5580
Aytuğ DEĞER <40007263+Aytgg@users.noreply.github.com>
2025-06-11 19:52:00 +0300

    Homepage & Auth pages added.

---
+++ frontend/app/routes/auth/login.tsx
@@ -0,0 +1,41 @@
+import {
+  type RouteConfig,
+  index,
+  route,
+  /*layout,*/
+  /*prefix,*/
+} from "@react-router/dev/routes";
+
+export default [
+  index("routes/home.tsx"),
+  route("login", "routes/auth/login.tsx"),
+  route("register", "routes/auth/register.tsx"),
+  // route("room/:roomId", "routes/waiting-in-room.tsx"),
+] satisfies RouteConfig;
+
+/*[
+  index("./home.tsx"),
+  route("about", "./about.tsx"),
+
+  layout("./auth/layout.tsx", [
+    route("login", "./auth/login.tsx"),
+    route("register", "./auth/register.tsx"),
+  ]),
+
+	route("teams/:teamId", "./team.tsx"),
+
+  ...prefix("concerts", [
+    index("./concerts/home.tsx"),
+    route(":city", "./concerts/city.tsx"),
+    route("trending", "./concerts/trending.tsx"),
+  ]),
+]*/
+
+/*[
+  // parent route
+  route("dashboard", "./dashboard.tsx", [
+    // child routes
+    index("./home.tsx"),
+    route("settings", "./settings.tsx"),
+  ]),
+]*/
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,12 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+import Input from "../components/common/Input";
+import Button from "../components/common/Button";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +15,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
-}
+  //return <Welcome />;
+  const [roomCode, setRoomCode] = useState("");
+  const navigate = useNavigate();
+
+  const handleJoin = () => (roomCode.length == 6 ? navigate(`/room/${roomCode}`) : alert("Room ID must be 6 characters long"));
+  const handleCreateQuiz = () => alert("Create Quiz functionality is not implemented yet");
+
+return (
+	<div style={{ padding: "20px", textAlign: "center" }}>
+		<h1>Quiz App</h1>
+		<Input value={roomCode} onChange={(e) => setRoomCode(e.target.value.toUpperCase().slice(0,6))} placeholder="Room ID"></Input>
+		<Button onClick={handleJoin} disabled={roomCode.length != 6}>Katıl</Button>
+		{!roomCode && <Button onClick={handleCreateQuiz}>Quiz Oluştur</Button>}
+	</div>
+);
+};
