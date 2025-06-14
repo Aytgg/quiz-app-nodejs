e08677a0b0c5c83ffc070ba7f80b2c7f6b987042
Aytuğ DEĞER <40007263+Aytgg@users.noreply.github.com>
2025-06-14 05:35:41 +0300

    DB bug fixed.

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
 
 initializeSocket(io);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("Server is running on port: " + process.env.PORT);
   });
